83a715c0e171c01d859581b0e2edd65cf97ac123
Snypintyme <gavingytse@gmail.com>
2025-03-24 19:18:25 -0400

    feat: add get user route

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,4 +1,5 @@
 """Flask application factory and configuration setup"""
+
 import os
 import logging
 from flask import Flask
@@ -89,10 +90,10 @@ def create_app():
     configure_logging(app)
 
     # Register blueprints
-    from app.auth.routes import auth_bp # pylint: disable=import-outside-toplevel
-    from app.main.routes import main_bp # pylint: disable=import-outside-toplevel
-    from app.users.routes import users_bp # pylint: disable=import-outside-toplevel
-    from app.image.routes import image_bp # pylint: disable=import-outside-toplevel
+    from app.auth.routes import auth_bp  # pylint: disable=import-outside-toplevel
+    from app.main.routes import main_bp  # pylint: disable=import-outside-toplevel
+    from app.users.routes import users_bp  # pylint: disable=import-outside-toplevel
+    from app.image.routes import image_bp  # pylint: disable=import-outside-toplevel
 
     app.register_blueprint(auth_bp, url_prefix="/api")
     app.register_blueprint(main_bp, url_prefix="/api")
