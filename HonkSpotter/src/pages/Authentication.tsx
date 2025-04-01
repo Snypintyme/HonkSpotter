241650de01caf980c1f21ec56124d2c42f45a6de
Snypintyme <gavingytse@gmail.com>
2025-04-01 19:10:10 -0400

    fix: fix Authentication file name

--- HonkSpotter/src/pages/Authentication.tsx
+++ HonkSpotter/src/pages/Authentication.tsx
@@ -7,7 +7,7 @@ interface AuthenticationProps {
 
 const Authentication = ({ authType }: AuthenticationProps) => {
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+    <div className="flex min-h-svh items-center w-full justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
         <AuthForm authType={authType} />
       </div>
