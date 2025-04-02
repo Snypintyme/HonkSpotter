aa88f025f4765f9bf952567b962b07a6227f6249
Snypintyme <gavingytse@gmail.com>
2025-04-02 01:43:50 -0400

    fix: navbar spacing

--- HonkSpotter/src/components/Navbar.tsx
+++ HonkSpotter/src/components/Navbar.tsx
@@ -77,18 +77,17 @@ const Navbar = () => {
 
   return (
     <nav className="bg-white shadow-sm">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16 border-b border-gray-200">
-          <div className="flex-shrink-0">
-            <span className="flex flex-row text-xl font-semibold text-gray-800">
-              <img src={logo} alt="Your Logo" className="h-8 w-auto" />
-              HonkSpotter
-            </span>
-          </div>
+          <Link to="/" className="text-gray-600 hover:text-gray-900">
+            <div className="flex-shrink-0">
+              <span className="flex flex-row text-xl font-semibold text-gray-800">
+                <img src={logo} alt="Your Logo" className="h-8 w-auto mr-2" />
+                HonkSpotter
+              </span>
+            </div>
+          </Link>
           <div className="flex items-center space-x-4">
-            <Link to="/" className="text-gray-600 hover:text-gray-900">
-              <Button variant="link">Home</Button>
-            </Link>
             <Button variant="default" onClick={handleTestApi}>
               Test API
             </Button>
