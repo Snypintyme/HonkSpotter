bb57c85c0edcfe43e255fe5661bbac71dc345890
Snypintyme <gavingytse@gmail.com>
2025-04-03 00:01:56 -0400

    fix: dates

--- HonkSpotter/src/components/SightingList.tsx
+++ HonkSpotter/src/components/SightingList.tsx
@@ -1,7 +1,7 @@
-import { navigateToSightingDetail } from '@/lib/utils';
+import { navigateToSightingDetail, toLocalDate } from '@/lib/utils';
 import { useCoordinatesStore } from '@/store/useCoordinatesStore';
 import { useGooseSightingStore } from '@/store/useGooseSightingStore';
-import { useEffect } from 'react';
+import { useEffect, useMemo } from 'react';
 
 const SightingList = () => {
   const { gooseSightings } = useGooseSightingStore();
@@ -13,11 +13,16 @@ const SightingList = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const sortedSightings = useMemo(
+    () => [...gooseSightings].sort((a, b) => new Date(b.created_date).getTime() - new Date(a.created_date).getTime()),
+    [gooseSightings]
+  );
+
   return (
     <>
       <h2 className="text-2xl font-bold mb-4">{`${gooseSightings.length} Reported Sightings`}</h2>
       <ul>
-        {gooseSightings.map((sighting, index) => (
+        {sortedSightings.map((sighting, index) => (
           <li
             key={index}
             className="mt-4 mb-4 border-b border-gray-300 pb-2 pl-2 cursor-pointer hover:bg-gray-100 transition duration-150 ease-in-out"
@@ -30,6 +35,7 @@ const SightingList = () => {
             <p className="text-sm text-gray-600">
               Coordinates: {sighting.coords.lat}, {sighting.coords.lng}
             </p>
+            <p className="text-sm text-gray-600">Reported On: {toLocalDate(sighting.created_date)}</p>
           </li>
         ))}
       </ul>
